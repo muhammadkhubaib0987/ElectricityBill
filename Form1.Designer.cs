fbc4756ecefe15b878dd5f68e9a3a1ee05d4af4a
PMYLS <PMYLS@DESKTOP-8BL3MIG>
2024-05-24 21:48:16 +0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace ElectricityBill
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.BtnDisplayStudent = new System.Windows.Forms.Button();
+            this.BtnDeleteStudent = new System.Windows.Forms.Button();
+            this.BtnUpdateStudent = new System.Windows.Forms.Button();
+            this.BtnInsertNewStudent = new System.Windows.Forms.Button();
+            this.LblAverageOfBillAtTheEndOfYear = new System.Windows.Forms.Label();
+            this.LblAmount = new System.Windows.Forms.Label();
+            this.LblReadingMonthName = new System.Windows.Forms.Label();
+            this.LblMeterNo = new System.Windows.Forms.Label();
+            this.LblCustomerName = new System.Windows.Forms.Label();
+            this.TxtAverageOfBillAtTheEndOfYear = new System.Windows.Forms.TextBox();
+            this.TxtAmount = new System.Windows.Forms.TextBox();
+            this.TxtReadingMonthName = new System.Windows.Forms.TextBox();
+            this.TxtMeterNo = new System.Windows.Forms.TextBox();
+            this.TxtCustomerName = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.groupBox1.Controls.Add(this.BtnDisplayStudent);
+            this.groupBox1.Controls.Add(this.BtnDeleteStudent);
+            this.groupBox1.Controls.Add(this.BtnUpdateStudent);
+            this.groupBox1.Controls.Add(this.BtnInsertNewStudent);
+            this.groupBox1.Controls.Add(this.LblAverageOfBillAtTheEndOfYear);
+            this.groupBox1.Controls.Add(this.LblAmount);
+            this.groupBox1.Controls.Add(this.LblReadingMonthName);
+            this.groupBox1.Controls.Add(this.LblMeterNo);
+            this.groupBox1.Controls.Add(this.LblCustomerName);
+            this.groupBox1.Controls.Add(this.TxtAverageOfBillAtTheEndOfYear);
+            this.groupBox1.Controls.Add(this.TxtAmount);
+            this.groupBox1.Controls.Add(this.TxtReadingMonthName);
+            this.groupBox1.Controls.Add(this.TxtMeterNo);
+            this.groupBox1.Controls.Add(this.TxtCustomerName);
+            this.groupBox1.Location = new System.Drawing.Point(23, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(687, 400);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // BtnDisplayStudent
+            // 
+            this.BtnDisplayStudent.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnDisplayStudent.Location = new System.Drawing.Point(536, 273);
+            this.BtnDisplayStudent.Name = "BtnDisplayStudent";
+            this.BtnDisplayStudent.Size = new System.Drawing.Size(144, 81);
+            this.BtnDisplayStudent.TabIndex = 3;
+            this.BtnDisplayStudent.Text = "Display Student";
+            this.BtnDisplayStudent.UseVisualStyleBackColor = true;
+            // 
+            // BtnDeleteStudent
+            // 
+            this.BtnDeleteStudent.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnDeleteStudent.Location = new System.Drawing.Point(373, 273);
+            this.BtnDeleteStudent.Name = "BtnDeleteStudent";
+            this.BtnDeleteStudent.Size = new System.Drawing.Size(144, 81);
+            this.BtnDeleteStudent.TabIndex = 3;
+            this.BtnDeleteStudent.Text = "Delete Student";
+            this.BtnDeleteStudent.UseVisualStyleBackColor = true;
+            // 
+            // BtnUpdateStudent
+            // 
+            this.BtnUpdateStudent.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnUpdateStudent.Location = new System.Drawing.Point(209, 273);
+            this.BtnUpdateStudent.Name = "BtnUpdateStudent";
+            this.BtnUpdateStudent.Size = new System.Drawing.Size(144, 81);
+            this.BtnUpdateStudent.TabIndex = 3;
+            this.BtnUpdateStudent.Text = "Update Student";
+            this.BtnUpdateStudent.UseVisualStyleBackColor = true;
+            // 
+            // BtnInsertNewStudent
+            // 
+            this.BtnInsertNewStudent.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnInsertNewStudent.Location = new System.Drawing.Point(41, 273);
+            this.BtnInsertNewStudent.Name = "BtnInsertNewStudent";
+            this.BtnInsertNewStudent.Size = new System.Drawing.Size(144, 81);
+            this.BtnInsertNewStudent.TabIndex = 3;
+            this.BtnInsertNewStudent.Text = "Insert New Student";
+            this.BtnInsertNewStudent.UseVisualStyleBackColor = true;
+
+            // 
+            // LblAverageOfBillAtTheEndOfYear
+            // 
+            this.LblAverageOfBillAtTheEndOfYear.AutoSize = true;
+            this.LblAverageOfBillAtTheEndOfYear.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblAverageOfBillAtTheEndOfYear.Location = new System.Drawing.Point(11, 217);
+            this.LblAverageOfBillAtTheEndOfYear.Name = "LblAverageOfBillAtTheEndOfYear";
+            this.LblAverageOfBillAtTheEndOfYear.Size = new System.Drawing.Size(342, 26);
+            this.LblAverageOfBillAtTheEndOfYear.TabIndex = 5;
+            this.LblAverageOfBillAtTheEndOfYear.Text = "Average Of Bills At End Of Year";
+            this.LblAverageOfBillAtTheEndOfYear.UseWaitCursor = true;
+            this.LblAverageOfBillAtTheEndOfYear.Click += new System.EventHandler(this.LblCustomerName_Click);
+            // 
+            // LblAmount
+            // 
+            this.LblAmount.AutoSize = true;
+            this.LblAmount.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblAmount.Location = new System.Drawing.Point(11, 172);
+            this.LblAmount.Name = "LblAmount";
+            this.LblAmount.Size = new System.Drawing.Size(94, 26);
+            this.LblAmount.TabIndex = 4;
+            this.LblAmount.Text = "Amount";
+            this.LblAmount.UseWaitCursor = true;
+            this.LblAmount.Click += new System.EventHandler(this.LblCustomerName_Click);
+            // 
+            // LblReadingMonthName
+            // 
+            this.LblReadingMonthName.AutoSize = true;
+            this.LblReadingMonthName.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblReadingMonthName.Location = new System.Drawing.Point(11, 124);
+            this.LblReadingMonthName.Name = "LblReadingMonthName";
+            this.LblReadingMonthName.Size = new System.Drawing.Size(237, 26);
+            this.LblReadingMonthName.TabIndex = 3;
+            this.LblReadingMonthName.Text = "Reading Month Name";
+            this.LblReadingMonthName.UseWaitCursor = true;
+            this.LblReadingMonthName.Click += new System.EventHandler(this.LblCustomerName_Click);
+            // 
+            // LblMeterNo
+            // 
+            this.LblMeterNo.AutoSize = true;
+            this.LblMeterNo.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblMeterNo.Location = new System.Drawing.Point(11, 76);
+            this.LblMeterNo.Name = "LblMeterNo";
+            this.LblMeterNo.Size = new System.Drawing.Size(111, 26);
+            this.LblMeterNo.TabIndex = 2;
+            this.LblMeterNo.Text = "Meter No";
+            this.LblMeterNo.UseWaitCursor = true;
+            this.LblMeterNo.Click += new System.EventHandler(this.LblCustomerName_Click);
+            // 
+            // LblCustomerName
+            // 
+            this.LblCustomerName.AutoSize = true;
+            this.LblCustomerName.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblCustomerName.Location = new System.Drawing.Point(11, 32);
+            this.LblCustomerName.Name = "LblCustomerName";
+            this.LblCustomerName.Size = new System.Drawing.Size(177, 26);
+            this.LblCustomerName.TabIndex = 1;
+            this.LblCustomerName.Text = "Customer Name";
+            this.LblCustomerName.UseWaitCursor = true;
+            this.LblCustomerName.Click += new System.EventHandler(this.LblCustomerName_Click);
+            // 
+            // TxtAverageOfBillAtTheEndOfYear
+            // 
+            this.TxtAverageOfBillAtTheEndOfYear.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.TxtAverageOfBillAtTheEndOfYear.Location = new System.Drawing.Point(373, 219);
+            this.TxtAverageOfBillAtTheEndOfYear.Name = "TxtAverageOfBillAtTheEndOfYear";
+            this.TxtAverageOfBillAtTheEndOfYear.Size = new System.Drawing.Size(308, 26);
+            this.TxtAverageOfBillAtTheEndOfYear.TabIndex = 1;
+            this.TxtAverageOfBillAtTheEndOfYear.UseWaitCursor = true;
+            // 
+            // TxtAmount
+            // 
+            this.TxtAmount.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.TxtAmount.Location = new System.Drawing.Point(373, 172);
+            this.TxtAmount.Name = "TxtAmount";
+            this.TxtAmount.Size = new System.Drawing.Size(308, 26);
+            this.TxtAmount.TabIndex = 1;
+            this.TxtAmount.UseWaitCursor = true;
+            // 
+            // TxtReadingMonthName
+            // 
+            this.TxtReadingMonthName.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.TxtReadingMonthName.Location = new System.Drawing.Point(373, 126);
+            this.TxtReadingMonthName.Name = "TxtReadingMonthName";
+            this.TxtReadingMonthName.Size = new System.Drawing.Size(308, 26);
+            this.TxtReadingMonthName.TabIndex = 1;
+            this.TxtReadingMonthName.UseWaitCursor = true;
+            // 
+            // TxtMeterNo
+            // 
+            this.TxtMeterNo.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.TxtMeterNo.Location = new System.Drawing.Point(373, 78);
+            this.TxtMeterNo.Name = "TxtMeterNo";
+            this.TxtMeterNo.Size = new System.Drawing.Size(308, 26);
+            this.TxtMeterNo.TabIndex = 1;
+            this.TxtMeterNo.UseWaitCursor = true;
+            // 
+            // TxtCustomerName
+            // 
+            this.TxtCustomerName.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.TxtCustomerName.Location = new System.Drawing.Point(373, 34);
+            this.TxtCustomerName.Name = "TxtCustomerName";
+            this.TxtCustomerName.Size = new System.Drawing.Size(308, 26);
+            this.TxtCustomerName.TabIndex = 1;
+            this.TxtCustomerName.UseWaitCursor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(716, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(340, 400);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(1057, 495);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label LblCustomerName;
+        private System.Windows.Forms.TextBox TxtCustomerName;
+        private System.Windows.Forms.Button BtnInsertNewStudent;
+        private System.Windows.Forms.Label LblAverageOfBillAtTheEndOfYear;
+        private System.Windows.Forms.Label LblAmount;
+        private System.Windows.Forms.Label LblReadingMonthName;
+        private System.Windows.Forms.Label LblMeterNo;
+        private System.Windows.Forms.TextBox TxtAverageOfBillAtTheEndOfYear;
+        private System.Windows.Forms.TextBox TxtAmount;
+        private System.Windows.Forms.TextBox TxtReadingMonthName;
+        private System.Windows.Forms.TextBox TxtMeterNo;
+        private System.Windows.Forms.Button BtnDisplayStudent;
+        private System.Windows.Forms.Button BtnDeleteStudent;
+        private System.Windows.Forms.Button BtnUpdateStudent;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,80 @@
+﻿using ModelLyr;
+using MySqlConnector;
+using System;
+using BusinessLyr;
+//using System.Data;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+using System.Diagnostics.Metrics;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ElectricityBill
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        MySqlConnection con = new MySqlConnection("server=localhost;port=3306;username=root;password='';database=bill");
+
+        private void LblCustomerName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnInsertNewStudent_Click(object sender, EventArgs e)
+        {
+            BillModel model = new BillModel()
+            {
+                CustomerName = TxtCustomerName.Text,
+                MeterNo = TxtMeterNo.Text,
+                ReadingMonthName = TxtReadingMonthName.Text,
+                Amount = TxtAmount.Text,
+                AverageOfBillAtTheEndOfYear = TxtAverageOfBillAtTheEndOfYear.Text
+            };
+            BillBusiness billBusiness = new BillBusiness();
+           billBusiness.AddBillBusiness(model);
+            MessageBox.Show("New Customer data Inserted Successflly");
+
+
+
+            /*1T  try
+            {
+                con.Open();
+                MessageBox.Show("connection open successfully");
+             
+                string query = "INSERT INTO electricitybill(CustomerName,MeterNo,ReadingMonthName,Amount,AverageOfBillsAtEndOfYear) VALUES(@CustomerName,@MeterNo,@ReadingMonthName,@Amount,@AverageOfBillsAtEndOfYear)";
+
+               MySqlCommand cmd = new MySqlCommand(query,con);
+               // MySqlCommand cmd = new MySqlCommand("AddBill", con);
+               // cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("CustomerName", TxtCustomerName.Text);
+                cmd.Parameters.AddWithValue("MeterNo", TxtCustomerName.Text);  
+                cmd.Parameters.AddWithValue("ReadingMonthName", TxtReadingMonthName.Text);
+                cmd.Parameters.AddWithValue("Amount", TxtAmount.Text);
+                cmd.Parameters.AddWithValue("AverageOfBillsAtEndOfYear" ,TxtAverageOfBillAtTheEndOfYear.Text);
+                cmd.Prepare();
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("New Customer data Inserted Successflly");
+                con.Close();
+
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show("error",Ex.Message);
+            }
+        }*/
+
+
+        }
+    }
+    }
+
+    
